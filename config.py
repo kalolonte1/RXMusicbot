e1f87b026f9cbef0129c95bcd9f4ecccf1401d54
kalolonte1 <89563449+kalolonte1@users.noreply.github.com>
2021-09-12 03:22:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Kennedy Music")
+BOT_NAME = getenv("BOT_NAME", "KGSUPERBOT")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/0f6f8a8a5ad69fe5ecf3d.png")
-BOT_IMG = getenv("BOT_IMAGE", "https://telegra.ph/file/d750cafe21a67044e8abd.jpg") 
-AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/f09189fdd97a3764a1f7a.jpg")
+BOT_IMG = getenv("BOT_IMAGE", "https://telegra.ph/file/07293ccbffed8887f1f71.jpg") 
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/07293ccbffed8887f1f71.jpg")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
@@ -21,7 +21,7 @@ ASSISTANT_NAME = getenv("ASSISTANT_NAME", "kennedyassistant")
 GROUP_SUPPORT = getenv("GROUP_SUPPORT", "zeusnihprend") 
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "rxsupportch") 
 OWNER_NAME = getenv("OWNER_NAME", "shutupbitchesss") # isi dengan username kamu tanpa simbol @
-DEV_NAME = getenv("DEV_NAME", "shutupbitchesss") 
+DEV_NAME = getenv("DEV_NAME", "knsgnwn") 
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "30"))
